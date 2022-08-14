02f5684684f51734754302413f09603c7a08024e
Juancito <cjuan.chona@hotmail.com>
2022-08-14 20:12:21 -0300

    Añadida funcion de creacion de los 500 usuarios clientes

--- sprint7/urls.py
+++ sprint7/urls.py
@@ -20,4 +20,5 @@ from django.urls import path,include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('itbank_page.urls')),
+    path('clients/',include('Clientes.urls'))
 ]
