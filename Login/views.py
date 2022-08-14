20eec3204be865471081fb609e93f41728a4ec48
Ignacio Latorre <ignaciolatorreap@gmail.com>
2022-08-14 20:53:59 -0300

    login + register added wip

--- Login/views.py
+++ Login/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import  render, redirect
+from .forms import NewUserForm
+from django.contrib.auth import login
+from django.contrib import messages
 
 # Create your views here.
+
+def register_request(request):
+	if request.method == "POST":
+		form = NewUserForm(request.POST)
+		if form.is_valid():
+			user = form.save()
+			login(request, user)
+			messages.success(request, "Registro exitoso." )
+			return redirect("/")
+		messages.error(request, "Registro fallido. Informacion invalida.")
+	form = NewUserForm()
+	return render (request = request, template_name = "registration/register.html", context = { "register_form": form })
--- itbank_page/views.py
+++ itbank_page/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(req):
+    
+
     return render(req,'home/home.html')
 
 def dolar(req):
